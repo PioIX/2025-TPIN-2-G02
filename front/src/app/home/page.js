5b4228eb3b89924b6a7b8816cd70864b1a51f906
perez <fperez@pioix.edu.ar>
2025-11-12 08:44:59 -0300

    a
    a

--- front/src/app/home/page.js
+++ front/src/app/home/page.js
@@ -29,38 +29,57 @@ export default function socketPage() {
 
         const handleUserJoined = (data) => {
             console.log("Usuario unido: ", data)
-            router.push("/jugador");
+        }
+
+        const handleCantJugadores = (data) => {
+            console.log("Cantidad de jugadores actualizada: ", data);
+            setSalas((prevSalas) =>
+                prevSalas.map((sala) =>
+                    sala.id_sala === data.id_sala
+                        ? { ...sala, cantidad_participantes: data.cantidad_participantes }
+                        : sala
+                )
+            );
         }
 
         socket.on("pingAll", handlePingAll);
         socket.on("newMessage", handleNewMessage);
         socket.on('userJoined', handleUserJoined);
+        socket.on('cantJugadores', handleCantJugadores);
 
         return () => {
             // Limpieza de eventos al desmontar el componente
             socket.off("pingAll", handlePingAll);
             socket.off("newMessage", handleNewMessage);
             socket.off('userJoined', handleUserJoined);
+            socket.off('cantJugadores', handleCantJugadores);
         };
     }, [socket]);
 
     async function getSalas() {
         let result = await fetch("http://localhost:4000/Salas");
         let response = await result.json();
-        console.log("SALAS!!",response)
+        console.log("SALAS!!", response)
         setSalas(response.sala);
     }
 
     function joinRoom(idSala) {
-        let sala = {}
+        let sala = {};
         if (isConnected) {
+            // Emitir evento para unirse a la sala
             socket.emit("joinRoom", { room: idSala, idLogged: idLogged });
-            for (let i=0; i<salas.length; i++) {
-                if(salas[i].idSala == idSala) {
-                    sala = salas[i]
+
+            // Actualizar la UI de las salas
+            for (let i = 0; i < salas.length; i++) {
+                if (salas[i].id_sala === idSala) {
+                    sala = salas[i];
+                    break;
                 }
             }
-            localStorage.setItem("sala", sala);
+            // Guardar en localStorage
+            localStorage.setItem("sala", JSON.stringify(sala));
+
+            // Redirigir al jugador
             router.push(`/jugador?idLogged=${idLogged}&room=${idSala}`);
         } else {
             console.error("Socket no está conectado");
@@ -80,10 +99,10 @@ export default function socketPage() {
                         <p>{sala.nombre}</p>
                         <p>{sala.cantidad_participantes} / {sala.max_jugadores} </p>
                         {
-                        (sala.cantidad_participantes < sala.max_jugadores) ? 
-                            <Button text={"Unirse"} onClick={() => joinRoom(sala.id_sala)} />
-                            :
-                            <p>Sala llena</p>
+                            (sala.cantidad_participantes < sala.max_jugadores) ?
+                                <Button text={"Unirse"} onClick={() => joinRoom(sala.id_sala)} />
+                                :
+                                <p>Sala llena</p>
                         }
                     </div>
                 ))}
